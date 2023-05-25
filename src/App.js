f277530da8c1126c18454de011644164c7e0b723
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-05-25 19:56:18 +0530

    changed ttle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Content from './Content';
-import Nav from './Nav';
-import Footer from './Footer';
+import Header from './HOME/Header';
+import Content from './HOME/Content';
+import Nav from './HOME/Nav';
+import Footer from './HOME/Footer';
+// import Login from './Login';
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Nav/>
-      <Content/>
-      <Footer/>
-    </div>
+      <div className="App">
+          <Header/>
+          <Nav/>
+          <Content/>
+          <Footer/>
+      </div>
   );
 }
 
 export default App;
+
+/* <Router>
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/contact" element={<Contact />} />
+</Routes>
+</Router> */
