8b6da28c51affe594b792936a1c857818b2f87c6
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-05-26 19:29:29 +0530

    completed login page

--- src/LOGIN/Login.js
+++ src/LOGIN/Login.js
@@ -10,18 +10,39 @@ function Login() {
     <div className='log'>
       <Header/>
       <Nav/>
-      <Link to="/">click for Home Page</Link>
-      <div className='log_in'>
-        <h2>Log in page under construction</h2>
-        <input type='text' />
-        <input type='text' />
-        <input type='text' />
-        <input type='text' />
+      <div class="form-container">
+        <form>
+          <h2>Log In </h2>
+          <div class="imgcontainer">
+            <img class="avatar" src='https://w7.pngwing.com/pngs/134/220/png-transparent-person-illustration-india-login-computer-icons-emoticon-medicine-user-login-icon-miscellaneous-text-logo-thumbnail.png'/>
+          </div>
+
+          <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required/>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required/>
+            
+            <button type="submit">Login</button>
+            <label>
+              <input type="checkbox" checked="checked" name="remember"/>
+              Remember me
+            </label>
+          </div>
+
+          <div class="container">
+            <Link class="gohome" to="/">Home Page</Link>
+          </div>
+          <div class="container">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <span class="pw">Forgot <a>Password?</a></span>
+          </div>
+        </form>
       </div>
       <Footer/>
-
     </div>
   )
 }
 
-export default Login
+export default Login;
