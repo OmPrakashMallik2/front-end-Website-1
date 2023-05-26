7ec67952d636bc47280627ccb8a0801212246fae
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-05-26 22:56:23 +0530

    working on Nav bar

--- src/Developer/Developer.js
+++ src/Developer/Developer.js
@@ -3,7 +3,7 @@ import './Developer.css';
 
 function Developer() {
   return (
-    <div className='dev'>
+    <div class='dev'>
       <img src="https://www.iconpacks.net/icons/1/free-user-icon-295-thumb.png" alt="Mera tasvir" />
       <h1>Om Prakash Mallik</h1>
       <h2>Reg N. 19105129032</h2>
--- src/HOME/Nav.js
+++ src/HOME/Nav.js
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <div class="Nav">
-      <Link class="btn" to="/">Home</Link>
-      <Link class="btn" to="/log">Log In</Link>
-      <Link class="btn" to="/developer">Developer</Link>
+      <ul>
+        <li><Link class="btn" to="/">Home</Link></li>
+        <li><Link class="btn" to="/developer">Developer</Link></li>
+        <li><Link class="btn" to="/log">Log In</Link></li>
+      </ul>
     </div>
   )
 }
 
-export default Nav
+export default Nav;
