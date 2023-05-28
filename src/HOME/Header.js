bd9442042afdb3d4e668f26fb7daaa0de6aa3aa8
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-05-28 16:59:43 +0530

    logo added

--- src/HOME/Header.js
+++ src/HOME/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 function Header() {
   return (
     <div class="Header">
+      <img class="web_logo" src="https://www.freepnglogos.com/uploads/logo-website-png/logo-website-website-icon-with-png-and-vector-format-for-unlimited-22.png" alt="logo"/>
       <h1>FRONT END PROJECT</h1>
-      <p>CREATED BY OM PRAKASH MALLIK</p>
     </div>
   )
 }
